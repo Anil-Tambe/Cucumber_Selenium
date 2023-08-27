68b4233111d57366c34d76ad056e2771b9bf3859
Anil_Tambe <anilat333@gmail.com>
2023-08-27 22:55:54 +0530

    added env variable to Env.java

--- src/main/java/util/Env.java
+++ src/main/java/util/Env.java
@@ -3,7 +3,7 @@ package util;
 import org.aeonbits.owner.Config;
 import org.aeonbits.owner.ConfigFactory;
 
-@Config.Sources("classpath:{env}.properties")
+@Config.Sources({"classpath:${env}.properties"})
 public interface Env extends Config {
     Env config = ConfigFactory.create(Env.class,System.getenv(),System.getProperties());
 
